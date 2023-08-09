340fce55667e0997f3026dbef2a47ce7110d31cd
Aashi <aashilahmad@gmail.com>
2023-08-08 23:44:10 -0400

    ReactCode

--- ReactCode/state-demo/src/ScoreKeeper2.jsx
+++ ReactCode/state-demo/src/ScoreKeeper2.jsx
@@ -1,6 +1,26 @@
 import { useState } from "react"
 export default function ScoreKeeper2({ numPlayers = 3, target = 5 }) {
-    const [scores, setScores ] = useState(new Array(numPlayers).fill(0));
+    const [scores, setScores] = useState(new Array(numPlayers).fill(0));
+    // const incrementScore = (idx) => {
+    //     setScores((prevScores) => {
+    //         const copy = [...prevScores];
+    //         copy[idx] += 1;
+    //         return copy;
+    //     });
+    // }
+    const incrementScore = (idx) => {
+        setScores((prevScores) => {
+            return prevScores.map((score,i) => {
+                if (i === idx) {
+                    return score + 1;
+                }
+                return score;
+            })
+        });
+    }
+    const reset = () => {
+        setScores(new Array(numPlayers).fill(0))
+    }
     return (
         <div>
             <h1>Score Keeper</h1>
@@ -9,10 +29,12 @@ export default function ScoreKeeper2({ numPlayers = 3, target = 5 }) {
                     return (
                         <li key={idx}>
                             Player{idx + 1}: {score}
-                            <button>+1</button>
+                            <button onClick={() => incrementScore(idx)}>+1</button>
+                            { score >= target && "Winner" }
                         </li>
                 )}) }
             </ul>
+            <button onClick={ reset}>Reset</button>
         </div>
     )
 }
