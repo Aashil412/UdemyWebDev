6f1ed40f136edbba8e8ac9cf4573f05fc80c2f3c
Aashi <aashilahmad@gmail.com>
2023-08-09 19:40:47 -0400

    ReactCode

--- ReactCode/state-demo/src/App.jsx
+++ ReactCode/state-demo/src/App.jsx
@@ -4,11 +4,26 @@ import Dumbo from './Dumbo';
 import ScoreKeeper from './ScoreKeeper';
 import EmojiClicker from './EmojiClicker';
 import ScoreKeeper2 from './ScoreKeeper2';
+import Lucky7 from './Lucky7';
+import LuckyN from './LuckyN';
+import { sum } from './utils';
+import BoxGrid from './BoxGrid';
+function lessthan4(dice) {
+  return sum(dice) < 4; 
+}
+
+function allSameValue(dice) {
+  return dice.every(v => v === dice[0]);
+}
+
 function App() {
   
   return (
     <>
-      <ScoreKeeper2 numPlayers={10} target={ 3} />
+      <BoxGrid/>
+      {/* <LuckyN winCheck={lessthan4} title="Roll less than 4"/>
+      <LuckyN winCheck={allSameValue} title ="Roll the same number"/> */}
+      {/* <ScoreKeeper2 numPlayers={10} target={ 3} /> */}
       {/* <EmojiClicker/> */}
       {/* <Counter/>
       <Dumbo/> */}
