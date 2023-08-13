2db8410278a13668cb35de61f48e815cf6bd5725
Aashi <aashilahmad@gmail.com>
2023-08-12 21:52:42 -0400

    ReactCode

---
+++ ReactCode/effects/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Counter from './counter'
+import QuoteFetcherLoader from './QuoteFetcherLoader'
+function App() {
+
+  return (
+    <>
+     <QuoteFetcherLoader/>
+    </>
+  )
+}
+
+export default App
