45eff1db8133c3f0a282dccd544a8475c7907722
Aashi <aashilahmad@gmail.com>
2023-08-12 22:27:16 -0400

    ReactCode

---
+++ ReactCode/material-ui-demo/src/FormDemo.jsx
@@ -0,0 +1,25 @@
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
+
+
+export default function FormDemo() {
+    const [name, setName] = useState("");
+    const [volume, setVolume] = useState(50)
+    const updateName = (e) => {
+        setName(e.target.value)
+    }
+    const changeVolume = (e, newval) => {
+        setVolume(newval);
+    }
+    return (
+        <Box sx={{border: '1px solid red', p:6, width: 300,height: 300, margin: "0 auto"}}>
+          <h1>Name is: { name}</h1>
+          <TextField id="outlined-basic" label="Puppy Name" variant="outlined" placeholder="Fido" value={name} onChange={updateName} />
+          <h2>Volume: {volume}</h2>
+          <Slider aria-label="Volume" value={volume} onChange={changeVolume} />
+
+    </Box>
+  );
+}
